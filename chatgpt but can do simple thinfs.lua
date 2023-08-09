bb19239b23c0b5e7ffc416aa851106f4f3ea0dec
shuttle-r <109020578+shuttle-r@users.noreply.github.com>
2023-08-09 18:04:47 +0800

    update chatgpt but can do simple thinfs.lua

--- chatgpt but can do simple thinfs.lua
+++ chatgpt but can do simple thinfs.lua
@@ -14,7 +14,7 @@
   move around automatically(not finish)
   can  reset
   set what ai should act(for Example you can make your ai act like a tsundere)
-  can equip and use the item in inventory(not finish)
+  can equip and use the item in inventory
   can add friend someone(not finish)
   can do jump
   um ai detect if player is staring at you
