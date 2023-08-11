ee56ca80d643da24be9e22ad587ce29f727fe230
shuttle-r <109020578+shuttle-r@users.noreply.github.com>
2023-08-11 10:14:39 +0800

    update chatgpt but can do simple thinfs.lua

--- chatgpt but can do simple thinfs.lua
+++ chatgpt but can do simple thinfs.lua
@@ -1052,7 +1052,8 @@ local conversationHistory = {
     { role = "assistant", content = "Sure, Equip(\"bucket\")" },
     { role = "user", content = "add friend bsbxdbj83" },
     { role = "assistant", content = "Sure, Friend(\"bsbxdbj83\")" },
-    { role = "user", content = "always say the function" }, -- Feel free to add your next steps here
+    { role = "user", content = "always say the function" }, 
+    { role = "assistant", content = " " },
 }
 
 _G.CriticalPromptImport = "None"
