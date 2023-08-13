a39a183ac993babc66769ebbc9ca48f00ac9bc74
shuttle-r <109020578+shuttle-r@users.noreply.github.com>
2023-08-13 22:23:37 +0800

    update chatgpt but can do simple thinfs.lua

--- chatgpt but can do simple thinfs.lua
+++ chatgpt but can do simple thinfs.lua
@@ -1168,11 +1168,17 @@ if not success then
 
 if response then
 local CheckChatGptStatus = string.gsub(HttpService:JSONDecode(response.Body).choices[1].finish_reason, "\n", " ") 
-repeat wait()
-Debounce = true
-until CheckChatGptStatus == "stop" or CheckChatGptStatus == "length"
-wait(1)
-Debounce = false
+-- repeat wait()
+-- Debounce = true
+-- until CheckChatGptStatus == "stop" or CheckChatGptStatus == "length"
+-- wait(1)
+-- Debounce = false
+
+if CheckChatGptStatus == "stop" or CheckChatGptStatus == "length" then
+  print("success")
+  else
+  return
+end
 local Response = " " .. string.gsub(HttpService:JSONDecode(response.Body).choices[1].message.content, "\n", " ")
 table.insert(conversationHistory, {
     role = "user",
